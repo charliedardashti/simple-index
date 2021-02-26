768fba47d228e304aa44d91e8dfaab8dad726284
subodhbisht <subodhbisht1989@gmail.com>
2021-02-26 14:24:16 +0530

    Duplicate invoice number check

--- src/main/java/com/simpleindex/scannerservice/service/impl/DocumentServiceImpl.java
+++ src/main/java/com/simpleindex/scannerservice/service/impl/DocumentServiceImpl.java
@@ -7,11 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Service("documentsService")
